1106484b4acb7d2a0a040585c5a0ed056c100f57
Justin Paoletta <justinpaoletta@gmail.com>
2020-05-15 15:25:06 -0400

    fix eyes deleted unused code

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -18,18 +18,18 @@ const TopRow = styled.div`
   z-index: 100;
 `;
 
-const Eyes = styled.img`
+const Eyes = styled.p`
   position:absolute;
-  height:1vh;
-  margin-top: 8px;
-  margin-left: -68px;
+  font-size: 4rem;
+  margin-left: 17px;
+  margin-top: -56px;
 `;
 
-const Eyes2 = styled.img`
+const Eyes2 = styled.p`
   position:absolute;
-  height:1vh;
-  margin-left: -50px;
-  margin-top: 8px;
+  font-size: 4rem;
+  margin-left: 36px;
+  margin-top: -56px;
 `;
 
 const TopIcons = styled.img`
@@ -157,7 +157,7 @@ class TopBar extends React.Component {
     if (textArea) {
       mySearch = (
         <SearchModal>
-          <Closemodal role="button" tabIndex={0} onKeyPress={() => {}} onClick={() => { this.setState({ textArea: false }); }}><CloseX alt="true" src={'https://myfecphotos.s3.amazonaws.com/x.jpg'} /></Closemodal>
+          <Closemodal role="button" tabIndex={0} onKeyPress={() => {}} onClick={() => { this.setState({ textArea: false }); }}><CloseX alt="true" src="https://myfecphotos.s3.amazonaws.com/x.jpg" /></Closemodal>
           <Searching placeholder="Search Recipes..." onChange={(e) => this.handleSearch(e.target.value)} />
           <br />
           <Findit href={fullsearch}>Find It</Findit>
@@ -166,15 +166,16 @@ class TopBar extends React.Component {
     }
     return (
       <TopRow>
-        <TopIcons src={'https://myfecphotos.s3.amazonaws.com/menu.svg'} alt={'https://myfecphotos.s3.amazonaws.com/menu.svg'} />
-        <Foodie>FOODIE
-        <Eyes alt="true" src="https://img.pngio.com/dot-png-images-free-download-black-dot-png-3500_3500.png" />
-        <Eyes2 alt="true" src="https://img.pngio.com/dot-png-images-free-download-black-dot-png-3500_3500.png" />
+        <TopIcons src="https://myfecphotos.s3.amazonaws.com/menu.svg" alt="https://myfecphotos.s3.amazonaws.com/menu.svg" />
+        <Foodie>
+          FOODIE
+          <Eyes>.</Eyes>
+          <Eyes2>.</Eyes2>
         </Foodie>
         <Right>
           <Recipes href="https://www.bonRecipesppetit.com/recipes">RECIPES</Recipes>
           <Login href="https://account.bonappetit.com/?brandSlug=bon-appetit&redirectUrl=https://www.bonappetit.com/&_ga=2.195360363.635364393.1588887829-1653893722.1588887829" className="login">{login}</Login>
-          <div role="button" tabIndex={0} onKeyPress={() => {}} onClick={() => { this.setState({ textArea: true }); }}><Search src={'https://myfecphotos.s3.amazonaws.com/search.svg'} alt={'https://myfecphotos.s3.amazonaws.com/search.svg'} /></div>
+          <div role="button" tabIndex={0} onKeyPress={() => {}} onClick={() => { this.setState({ textArea: true }); }}><Search src="https://myfecphotos.s3.amazonaws.com/search.svg" alt="https://myfecphotos.s3.amazonaws.com/search.svg" /></div>
           {mySearch}
         </Right>
       </TopRow>
