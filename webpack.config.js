0878f632b421370a6b6411a0a4b6f722aa18c504
Justin Paoletta <justinpaoletta@gmail.com>
2020-05-15 17:21:27 -0400

    compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 const path = require('path');
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
 
 module.exports = {
 
-  mode: 'production',
+  plugins: [
+    new BrotliGzipPlugin({
+      asset: '[path].br[query]',
+      algorithm: 'brotli',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      quality: 11,
+    }),
+    new BrotliGzipPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
 
   optimization: {
     nodeEnv: 'production',
@@ -19,6 +36,8 @@ module.exports = {
   resolve: { extensions: ['.js', '.jsx'] },
 
   module: {
+
+
     rules: [
       {
         test: /\.js$|jsx/,
@@ -36,8 +55,15 @@ module.exports = {
         loader: 'style-loader!css-loader',
       },
       {
-        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000',
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: false,
+            },
+          },
+        ],
       },
     ],
   },
