c1f576e70955bc393e1fc48fbb2a8d90e79825d1
Justin Paoletta <justinpaoletta@gmail.com>
2020-05-16 11:28:50 -0400

    fix password

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'justin',
-  pasword: 'codecode',
+  password: 'codecode',
   database: 'myFEC',
 });
 
